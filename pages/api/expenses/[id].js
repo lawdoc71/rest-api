eedd51655e94d30254b099270bab3d77d5e3552d
Mikedbutler <michael@butlerlegaltech.com>
2022-05-25 08:24:30 -0700

    sync

---
+++ pages/api/expenses/[id].js
@@ -0,0 +1,42 @@
+import prisma from 'lib/prisma'
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const expense = await prisma.expense.findUnique({
+      where: {
+        id: parseInt(req.query.id),
+      },
+    })
+    if (!expense) {
+      return res.status(404).json({ message: 'Not Found' })
+    }
+    res.status(200).json({ ...expense })
+  }
+  if (req.method === 'PUT') {
+    const { trip, name, date, amount, currency } = req.body
+
+    await prisma.expense.update({
+      data: {
+        trip,
+        name,
+        date,
+        amount,
+        currency,
+      },
+      where: {
+        id: parseInt(req.query.id),
+      },
+    })
+
+    return res.status(200).end()
+  }
+  if (req.method === 'DELETE') {
+    await prisma.expense.delete({
+      where: {
+        id: parseInt(req.query.id),
+      },
+    })
+  
+    return res.status(200).end()
+  }
+}
---
+++ pages/api/trips/[id].js
@@ -0,0 +1,42 @@
+import prisma from 'lib/prisma'
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const trip = await prisma.trip.findUnique({
+        where: {
+          id: parseInt(req.query.id),
+        },
+      })
+      if (!trip) {
+        return res.status(404).json({ message: 'Not Found' })
+      }
+      res.status(200).json(trip)
+  }
+
+	if (req.method === 'PUT') {
+    const { user, name, start_date, end_date } = req.body
+
+    await prisma.trip.update({
+      data: {
+        user,
+        name,
+        start_date,
+        end_date,
+      },
+      where: {
+        id: parseInt(req.query.id),
+      },
+    })
+
+    return res.status(200).end()
+  }
+	if (req.method === 'DELETE') {
+    await prisma.trip.delete({
+      where: {
+        id: parseInt(req.query.id),
+      },
+    })
+
+    return res.status(200).end()
+  }
+}
